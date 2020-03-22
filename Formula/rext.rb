23514df26a40bd01f03fe2643b81d031cd366789
Bryce Campbell <tonyhawk2100@gmail.com>
2020-03-21 21:11:18 -0700

    fix typo.

--- Formula/rext.rb
+++ Formula/rext.rb
@@ -2,8 +2,7 @@ class Rext < Formula
   desc "tool that can quickly change the extension of many files as simply as it is to use mv to rename a single file."
   homepage ""
   url "https://github.com/bryceac/rext/archive/rext-0.1.tar.gz"
-  sha256 "f5bd09489e397f15d5e0f885c241a79cc90d83805f08afbfca605e29759b921c
-  "
+  sha256 "f5bd09489e397f15d5e0f885c241a79cc90d83805f08afbfca605e29759b921c"
 
   def install
     system "make", "install", "prefix=#{prefix}"
